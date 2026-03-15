0d89d6a463591f1d85600762a09190761a772eb4
ViniiR <cvrm2004@gmail.com>
2026-03-15 11:37:33 -0300

    init

---
+++ mod/src/main/java/com/vinii/harderprogression/HarderProgressionClient.java
@@ -0,0 +1,10 @@
+package com.vinii.harderprogression;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarderProgressionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ mod/src/main/java/com/vinii/harderprogression/HarderProgressionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.vinii.harderprogression;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HarderProgressionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
